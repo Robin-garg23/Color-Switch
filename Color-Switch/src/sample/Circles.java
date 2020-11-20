7ff9db0abdadbe440c0d69fafa35f73ad5ae4ba7
Robin-garg23 <robin19092@iiitd.ac.in>
2020-11-21 01:04:54 +0530

    Added shapes

--- Color-Switch/src/sample/Circles.java
+++ Color-Switch/src/sample/Circles.java
@@ -7,9 +7,11 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,16 +23,24 @@ import java.sql.SQLOutput;
 
 
 public class Circles extends Application {
-    @FXML
-    Pane circlepane;
+//    @FXML
+//    VBox circlepane;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
 
-        Parent root = FXMLLoader.load(getClass().getResource("Circle.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Cross.fxml"));
+        RotateTransition rotateTransition=new RotateTransition(Duration.seconds(4),root);
+
+        rotateTransition.setByAngle(360);
+        rotateTransition.setAxis(Rotate.Z_AXIS);
+        rotateTransition.setCycleCount(400);
+        rotateTransition.play();
         primaryStage.setTitle("Color Switch");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root1 = new Group();
+        root1.getChildren().add(root);
+        primaryStage.setScene(new Scene(root1, 300, 275,Color.BLACK));
         primaryStage.show();
     }
 //    @FXML
@@ -44,13 +54,15 @@ public class Circles extends Application {
         System.out.println("checkcheck");
         launch(args);
     }
-    @FXML
-    public void rotata(MouseEvent mouseEvent) {
-        RotateTransition rotateTransition=new RotateTransition(Duration.seconds(5),circlepane);
-        rotateTransition.setFromAngle(0);
-        rotateTransition.setByAngle(360);
-        rotateTransition.setCycleCount(400);
-        rotateTransition.play();
-
-    }
+//    @FXML
+//    public void rotata(MouseEvent mouseEvent) {
+//        circlepane.setMaxHeight(250);
+//        circlepane.setMaxWidth(250);
+//        RotateTransition rotateTransition=new RotateTransition(Duration.seconds(1),circlepane);
+//        rotateTransition.setFromAngle(0);
+//        rotateTransition.setByAngle(360);
+//        rotateTransition.setCycleCount(400);
+//        rotateTransition.play();
+//
+//    }
 }
