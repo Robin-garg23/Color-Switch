ddeb6db3f949a764571605026604fcc68be872e9
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-11-21 16:41:37 +0530

    NewMainPage

--- Color-Switch/src/sample/Circles.java
+++ Color-Switch/src/sample/Circles.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.animation.*;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -26,9 +27,15 @@ public class Circles extends Application {
     public void start(Stage primaryStage) throws Exception{
 
 
-        StackPane root = FXMLLoader.load(getClass().getResource("concircle.fxml"));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("NewMainPage.fxml"));
         List<Node> parentChildren = root.getChildren();
-        RotateTransition rotateTransition=new RotateTransition(Duration.millis(4000),parentChildren.get(0));
+
+
+        StackPane first=(StackPane)parentChildren.get(5);
+        StackPane second=(StackPane)parentChildren.get(6);
+        List<Node> firstone=first.getChildren();
+        List<Node> secondone=second.getChildren();
+        RotateTransition rotateTransition=new RotateTransition(Duration.millis(4000),firstone.get(0));
         rotateTransition.setFromAngle(0);
         rotateTransition.setToAngle(360);
         rotateTransition.setInterpolator(Interpolator.LINEAR);
@@ -36,7 +43,7 @@ public class Circles extends Application {
         rotateTransition.setCycleCount(Timeline.INDEFINITE);
         rotateTransition.setAutoReverse(false);
         rotateTransition.play();
-        RotateTransition rotateTransition1=new RotateTransition(Duration.millis(4000),parentChildren.get(1));
+        RotateTransition rotateTransition1=new RotateTransition(Duration.millis(4000),firstone.get(1));
         rotateTransition1.setFromAngle(360);
         rotateTransition1.setToAngle(0);
         rotateTransition1.setInterpolator(Interpolator.LINEAR);
@@ -44,10 +51,42 @@ public class Circles extends Application {
         rotateTransition1.setCycleCount(Timeline.INDEFINITE);
         rotateTransition1.setAutoReverse(false);
         rotateTransition1.play();
+        RotateTransition rotateTransition2=new RotateTransition(Duration.millis(4000),secondone.get(0));
+        rotateTransition2.setFromAngle(0);
+        rotateTransition2.setToAngle(360);
+        rotateTransition2.setInterpolator(Interpolator.LINEAR);
+        rotateTransition2.setAxis(Rotate.Z_AXIS);
+        rotateTransition2.setCycleCount(Timeline.INDEFINITE);
+        rotateTransition2.setAutoReverse(false);
+        rotateTransition2.play();
+        RotateTransition rotateTransition3=new RotateTransition(Duration.millis(4000),secondone.get(1));
+        rotateTransition3.setFromAngle(360);
+        rotateTransition3.setToAngle(0);
+        rotateTransition3.setInterpolator(Interpolator.LINEAR);
+        rotateTransition3.setAxis(Rotate.Z_AXIS);
+        rotateTransition3.setCycleCount(Timeline.INDEFINITE);
+        rotateTransition3.setAutoReverse(false);
+        rotateTransition3.play();
+        RotateTransition rotateTransition4=new RotateTransition(Duration.millis(4000),parentChildren.get(7));
+        rotateTransition4.setFromAngle(360);
+        rotateTransition4.setToAngle(0);
+        rotateTransition4.setInterpolator(Interpolator.LINEAR);
+        rotateTransition4.setAxis(Rotate.Z_AXIS);
+        rotateTransition4.setCycleCount(Timeline.INDEFINITE);
+        rotateTransition4.setAutoReverse(false);
+        rotateTransition4.play();
+        RotateTransition rotateTransition5=new RotateTransition(Duration.millis(4000),parentChildren.get(8));
+        rotateTransition5.setFromAngle(360);
+        rotateTransition5.setToAngle(0);
+        rotateTransition5.setInterpolator(Interpolator.LINEAR);
+        rotateTransition5.setAxis(Rotate.Z_AXIS);
+        rotateTransition5.setCycleCount(Timeline.INDEFINITE);
+        rotateTransition5.setAutoReverse(false);
+        rotateTransition5.play();
         primaryStage.setTitle("Color Switch");
         Group root1 = new Group();
         root1.getChildren().add(root);
-        primaryStage.setScene(new Scene(root1, 300, 275,Color.BLACK));
+        primaryStage.setScene(new Scene(root1, 500, 275,Color.BLACK));
         primaryStage.show();
     }
 //    @FXML
