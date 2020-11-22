10bd347e1e10f1c4b5b2b1e1cb7f9aff776e29e8
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-11-22 16:56:16 +0530

    gameover fxml

---
+++ Color-Switch/src/sample/gameOver.java
@@ -0,0 +1,37 @@
+package sample;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.List;
+
+public class gameOver {
+
+    public AnchorPane root;
+
+    gameOver() throws IOException {
+        root = FXMLLoader.load(getClass().getResource("gameOver.fxml"));
+    }
+
+
+}
+
+
+
+
+
+
+
